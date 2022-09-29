ec86cb18698a904b409f7408b054cd0ef22a60ab
Oliver Hacker <oliver-hacker@web.de>
2022-09-29 21:54:43 +0200

    Add test cases for possible failure points

--- src/test/java/de/exxcellent/challenge/input/csv/CsvReaderTest.java
+++ src/test/java/de/exxcellent/challenge/input/csv/CsvReaderTest.java
@@ -1,19 +1,21 @@
 package de.exxcellent.challenge.input.csv;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.StringReader;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
 import java.util.List;
-import java.util.ResourceBundle;
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class CsvReaderTest {
@@ -61,6 +63,55 @@ public class CsvReaderTest {
         assertEquals("some text", result.get(0).secondColumn);
     }
 
+    @Test
+    public void testEmptySource() {
+        String input = "";
+        var csvReader = new CsvReader<>(',', new StringReader(input), DataObject.class);
+        assertThrows(IllegalStateException.class, csvReader::readData);
+    }
+
+    @Test
+    public void testEmptyName() {
+        String input = "first,\n42.3,some text";
+        var csvReader = new CsvReader<>(',', new StringReader(input), DataObject.class);
+        assertThrows(IllegalStateException.class, csvReader::readData);
+    }
+
+    @Test
+    public void testMismatchingNumberOfColumns() {
+        String input = "first,second,third\n42.3,some text";
+        var csvReader = new CsvReader<>(',', new StringReader(input), DataObject.class);
+        assertThrows(IllegalStateException.class, csvReader::readData);
+    }
+
+    @Test
+    public void testInconsistentNames() {
+        String input = "first,second\n42.3,some text";
+        var csvReader = new CsvReader<>(',', new StringReader(input), DataObject.class);
+        assertThrows(NullPointerException.class, csvReader::readData);
+    }
+
+    @Test
+    public void testHasAnyAnnotations() {
+        String input = "first,second\n42.3,some text";
+        var csvReader = new CsvReader<>(',', new StringReader(input), NoAnnotations.class);
+        assertThrows(IllegalStateException.class, csvReader::readData);
+    }
+
+    @Test
+    public void testUniqueNames() {
+        String input = "first,second\n42.3,some text";
+        var csvReader = new CsvReader<>(',', new StringReader(input), NonUniqueNames.class);
+        assertThrows(IllegalStateException.class, csvReader::readData);
+    }
+
+    @Test
+    public void testUnsupportedType() {
+        String input = "field\n42.3";
+        var csvReader = new CsvReader<>(',', new StringReader(input), UnsupportedFieldType.class);
+        assertThrows(IllegalStateException.class, csvReader::readData);
+    }
+
     static class DataObject {
 
         @CsvColumn(columnName = "the first one")
@@ -68,6 +119,23 @@ public class CsvReaderTest {
         @CsvColumn(columnName = "the/second$one")
         String secondColumn;
 
+    }
+
+    static class NoAnnotations {
+        float first;
+        String second;
+    }
+
+    static class NonUniqueNames {
+        @CsvColumn(columnName = "field")
+        float field;
+        @CsvColumn(columnName = "field")
+        double another;
+    }
+
+    static class UnsupportedFieldType {
+        @CsvColumn(columnName = "field")
+        IntStream field;
     }
 
 }
