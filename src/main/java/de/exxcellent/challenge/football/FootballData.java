35909b28cde1ad8d901d3aa83ed0f5cc983b6b2f
Oliver Hacker <oliver-hacker@web.de>
2022-09-29 23:13:52 +0200

    Add FootballData class

---
+++ src/main/java/de/exxcellent/challenge/football/FootballData.java
@@ -0,0 +1,25 @@
+package de.exxcellent.challenge.football;
+
+import de.exxcellent.challenge.input.csv.CsvColumn;
+
+
+public class FootballData {
+
+    @CsvColumn(columnName = "Team")
+    public String team;
+    @CsvColumn(columnName = "Games")
+    public int games;
+    @CsvColumn(columnName = "Wins")
+    public int wins;
+    @CsvColumn(columnName = "Losses")
+    public int losses;
+    @CsvColumn(columnName = "Draws")
+    public int draws;
+    @CsvColumn(columnName = "Goals")
+    public int goals;
+    @CsvColumn(columnName = "Goals Allowed")
+    public int goals_allowed;
+    @CsvColumn(columnName = "Points")
+    public int points;
+
+}
