32d23a6c32b13bdba79400974a338f61d008c88d
Oliver Hacker <oliver-hacker@web.de>
2022-09-29 23:50:58 +0200

    Fix documentation

--- src/main/java/de/exxcellent/challenge/util/ListUtil.java
+++ src/main/java/de/exxcellent/challenge/util/ListUtil.java
@@ -15,12 +15,18 @@ public class ListUtil {
      * Find the entry in the given list associated with the minimum. Values for each item are
      * computed using the given function and then compared.
      *
-     * @param data The data to base calculation off of
-     * @param keyComp A function that takes a data element and produces a key by which the
-     *                data can be ordered.
-     * @return The element associated with the minimum - or {@code null}
-     * @param <T> The type of data
-     * @param <N> The key type
+     * @param data
+     *         The data to base calculation off of
+     * @param keyComp
+     *         A function that takes a data element and produces a key by which the data can be
+     *         ordered.
+     * @param <T>
+     *         The type of data
+     * @param <N>
+     *         The key type
+     *
+     * @return The element associated with the minimum - or an empty {@link Optional} if none
+     * exists.
      */
     public static <T, N extends Comparable<? super N>> Optional<T> argmin(List<? extends T> data,
                                                                           Function<? super T, N> keyComp) {
